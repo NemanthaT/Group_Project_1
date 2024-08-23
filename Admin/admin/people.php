f6c0a5db36d611ce56f4794dc34c70ff8fa35345
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-08-24 00:57:45 +0530

    refactor: Add admin/people.php page for displaying all users

---
+++ Admin/admin/people.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="people.css">
+    </head>
+
+    <body>
+        <h1>All users</h1>
+        <table>
+            <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+        </table>
+    </body>
+</html>
