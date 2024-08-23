ef4bdb93ce68808cca70359b52001018887f57ac
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-08-24 02:02:28 +0530

    feat: Add clients page for displaying client information

---
+++ Admin/admin/Users/clients.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Clients</title>
+        <link rel="stylesheet" href="people.css">
+    </head>
+
+    <body>
+        <h1>Clients</h1>
+        <table>
+            <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>jhon@gmail.com</td>
+                <td>Client</td>
+            </tr>
+        </table>
+    </body>
+</html>
