136c2990bacb33cce665b0f82c415c0b8f953d17
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-08-24 02:28:51 +0530

    refactor: Update admin.php to add new navigation links

--- Admin/admin/admin.php
+++ Admin/admin/admin.php
@@ -28,8 +28,13 @@
                 </div>
                 <nav>
                     <ul>
-                        <li class="dropDown"><a href="dashboard.html" target="Dashboard">Dashboard</a></li>
-                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">People</a></li>
+                        <li class="dropDown"><a href="dashboard.php" target="Dashboard">Dashboard</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Users</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Service Provider Requests</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Forums</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Financial Reports</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Appointments</a></li>
+                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Manage Employees</a></li>
                     </ul>
                 </nav>
             </div>
@@ -43,7 +48,7 @@
                     </header>
                 </div>
                 <div class="frame">
-                    <iframe src="dashboard.html" name="Dashboard" title="Dashboard"></iframe>
+                    <iframe src="dashboard.php" name="Dashboard" title="Dashboard"></iframe>
                 </div>
             </div>
         </div>
