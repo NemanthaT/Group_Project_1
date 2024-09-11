30127bc51cb80e37b00040624500e97dc16ce4cc
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-09-11 18:47:54 +0530

    refactor: Improve forums.php table layout and styling

---
+++ Admin/admin/Forums/forums.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Clients</title>
+        <link rel="stylesheet" href="forums.css">
+    </head>
+
+    <body>
+        <h1>Manage Forums</h1>
+        <center>
+            <table>
+                <tr>
+                    <th>Forum Id</th>
+                    <th>Title</th>
+                    <th>By</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>General</td>
+                    <td>Kasun</td>
+                    <td>
+                        <button>View</button>
+                        <button>Approve</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>News</td>
+                    <td>Nimal</td>
+                    <td>
+                        <button>View</button>
+                        <button>Approve</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Help</td>
+                    <td>Amal</td>
+                    <td>
+                        <button>View</button>
+                        <button>Approve</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>General Discussion</td>
+                    <td>Vimal</td>
+                    <td>
+                        <button>View</button>
+                        <button>Approve</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </body>
+</html>
