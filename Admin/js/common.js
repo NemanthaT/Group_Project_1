60d93e48d5ebdb909bac93997b3cdf84bb0fccb4
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-09-11 16:37:22 +0530

    refactor: Add loadPage function in common.js

--- Admin/js/common.js
+++ Admin/js/common.js
@@ -2,3 +2,21 @@
 function redirectTo(url) {
     window.location.href = url;
 }
+
+// Load page
+function loadPage(url) {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Page not found');
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById('frame').innerHTML = data;
+      })
+      .catch(error => {
+        document.getElementById('frame').innerHTML = '<p>Error loading the page</p>';
+        console.error('There was an error:', error);
+      });
+  }
