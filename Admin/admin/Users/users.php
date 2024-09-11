eef1b72ba09f75ac4aa9208327378be4b1a8b6d1
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-09-11 15:35:35 +0530

    refactor: Update users.php to improve admin dashboard layout and navigation links

--- Admin/admin/Users/users.php
+++ Admin/admin/Users/users.php
@@ -7,19 +7,22 @@
         <link rel="stylesheet" href="users.css">
     </head>
     <body>
-            <div class="dashboard">
-                <div class="card">
-                    <div class="icon"></div>
-                    <p><a href="clients.php">Clients</a></p>
-                </div>
-                <div class="card">
-                    <div class="icon"></div>
-                    <p><a href="serviceProviders.php">Service Providers</a></p>
-                </div>
-                <div class="card">
-                    <div class="icon"></div>
-                    <p><a href="companyWorkers.php">Company Workers</a></p>
-                </div>
+        <div class="header">
+            <h1>Users</h1>
+        </div>
+        <div class="dashboard">
+            <div class="card">
+                <div class="icon"></div>
+                <p><a href="clients.php">Clients</a></p>
             </div>
+            <div class="card">
+                <div class="icon"></div>
+                <p><a href="serviceProviders.php">Service Providers</a></p>
+            </div>
+            <div class="card">
+                <div class="icon"></div>
+                <p><a href="companyWorkers.php">Company Workers</a></p>
+            </div>
+        </div>
     </body>
 </html>
