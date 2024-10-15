8a09c814151c1ccbcfb0f2039f66a4b14ef4d93f
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-10-15 20:18:22 +0530

    Refactor: Update page title in manEmp.php

--- Admin/admin/ManageEmployees/manEmp.php
+++ Admin/admin/ManageEmployees/manEmp.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Live Search</title>
+    <title>Manage Employees</title>
     <link rel="stylesheet" href="../Users/people.css">
     <link rel="stylesheet" href="mngEmp.css">
 </head>
