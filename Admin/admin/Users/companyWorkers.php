6421cd264e2d498da5f32e97972d6e61f8506835
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-04 00:46:51 +0530

    Style: Update companyWorkers.php; change stylesheet link from people.css to peopleStyles.css for consistency

--- Admin/admin/Users/companyWorkers.php
+++ Admin/admin/Users/companyWorkers.php
@@ -21,11 +21,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Company Workers</title>
-        <link rel="stylesheet" href="people.css">
+        <link rel="stylesheet" href="peopleStyles.css">
     </head>
 
     <body>
-        <h1><u>Company Workers</u></h1>
+        <h1>Company Workers</h1>
         <center>
             <table>
                 <tr>
