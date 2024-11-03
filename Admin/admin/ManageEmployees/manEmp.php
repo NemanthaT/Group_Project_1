42f9ab7b29a4bf9384db4ab436e6384c01fc74be
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-03 23:56:15 +0530

    Style: Update stylesheet link in manEmp.php; switch to empStyles.css for employee management

--- Admin/admin/ManageEmployees/manEmp.php
+++ Admin/admin/ManageEmployees/manEmp.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Employees</title>
-    <link rel="stylesheet" href="mngEmp.css">
+    <link rel="stylesheet" href="empStyles.css">
 </head>
 <body>
     <center><h1>Manage Employees</h1></center>
