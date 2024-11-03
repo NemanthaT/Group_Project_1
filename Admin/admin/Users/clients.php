8a9fa55a275abf5a848afe232490d5914710500c
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-04 00:46:30 +0530

    Style: Update clients.php; change stylesheet link from people.css to peopleStyles.css for consistency

--- Admin/admin/Users/clients.php
+++ Admin/admin/Users/clients.php
@@ -21,7 +21,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Clients</title>
-        <link rel="stylesheet" href="people.css">
+        <link rel="stylesheet" href="peopleStyles.css">
     </head>
 
     <body>
