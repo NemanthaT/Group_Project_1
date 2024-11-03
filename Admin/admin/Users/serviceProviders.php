94979c7ba57216205917505925ae2ee0766d6e1c
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-04 00:47:03 +0530

    Style: Update serviceProviders.php; change stylesheet link from people.css to peopleStyles.css for consistency

--- Admin/admin/Users/serviceProviders.php
+++ Admin/admin/Users/serviceProviders.php
@@ -21,7 +21,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Service Providers</title>
-        <link rel="stylesheet" href="people.css">
+        <link rel="stylesheet" href="peopleStyles.css">
     </head>
 
     <body>
