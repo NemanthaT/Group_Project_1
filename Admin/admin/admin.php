495185f62c28f215c30824df3c78cc28d6561184
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-13 22:31:48 +0530

    Fix: Remove redundant Appointments link from admin navigation

--- Admin/admin/admin.php
+++ Admin/admin/admin.php
@@ -35,7 +35,6 @@
                         <li class="dropDown"><a href="Requests/requests.php" target="Dashboard">Service Provider Requests</a></li>
                         <li class="dropDown"><a href="Forums/forums.php" target="Dashboard">Forums</a></li>
                         <li class="dropDown"><a href="Users/users.php" target="Dashboard">Financial Reports</a></li>
-                        <li class="dropDown"><a href="Users/users.php" target="Dashboard">Appointments</a></li>
                         <li class="dropDown"><a href="ManageEmployees/manEmp.php" target="Dashboard">Manage Employees</a></li>
                     </ul>
                 </nav>
