b3ed1bef6beba3212f9f9b2747dbae75fd26ae8f
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-13 22:35:26 +0530

    Js file for request.php

---
+++ Admin/admin/Requests/requests.js
@@ -0,0 +1,62 @@
+function viewReq(id) {
+    // Send an AJAX request to the PHP script
+    fetch('view_req.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `id=${id}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Check if data contains error
+        if (data.error) {
+            alert(data.error);
+        } else {
+            // Display forum details in the designated area
+            document.getElementById('hiddenView').style.display = "block";
+            document.getElementById("reqId").innerText = data.reqId;
+            document.getElementById("reqName").innerText = data.full_name;
+            document.getElementById("reqEmail").innerText = data.email;
+            document.getElementById("reqTel").innerText = data.phone;
+            document.getElementById("reqField").innerText = data.field;
+            document.getElementById("reqSpec").innerText = data.specialty;
+        }
+    })
+    .catch(error => console.error('Error fetching forum data:', error));
+}
+
+function deleteReq(id) {
+    if (confirm("Are you sure you want to delete this item?")) {
+        // Proceed with delete action
+            // Send an AJAX request to the PHP script
+        fetch('delete_req.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `id=${id}`
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Check if data contains error
+            if (data.error) {
+                alert(data.error);
+            } else {
+                // Reload the page
+                location.reload();
+            }
+        })
+        .catch(error => console.error('Error deleting forum:', error));
+        alert("Item deleted.");
+        window.location.href = forums.php;
+    } else {
+        // Do nothing
+        alert("Delete canceled.");
+    }
+
+}
+
+function closeView(){
+    document.getElementById('hiddenView').style.display = "none";
+}
