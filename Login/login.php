17fe5d9a741e0e2a539dee2daa59492ea0e6f76d
Safran Zahim <safran1600014@gmail.com>
2024-11-23 13:08:29 +0530

    add session management to login and dashboard for user authentication

--- Login/login.php
+++ Login/login.php
@@ -1,5 +1,7 @@
 <?php
+        
 session_start();
+
 include '../connect/connect.php'; //connect to database
 
 if (isset($_POST['submit'])) { //check if form was submitted
--- user/Dashboard/Dashboard.php
+++ user/Dashboard/Dashboard.php
@@ -1,3 +1,16 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['email']))
+{
+    header("Location: ../../Login/Logout.php");
+    exit();
+}
+else {
+
+    
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
