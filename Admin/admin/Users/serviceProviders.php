0c99b5d7e2c181b7a79c15a229fe95f7b50039ea
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-11-26 02:09:42 +0530

    Fix: Refactor serviceProviders.php layout for improved structure and readability

--- Admin/admin/Users/serviceProviders.php
+++ Admin/admin/Users/serviceProviders.php
@@ -27,47 +27,49 @@
     </head>
 
     <body>
-        <div class="bg">
-                <!--blur Background image-->  
-        </div> 
-        <div>
-            <h1>Service Providers</h1>
-            <div id="hiddenView">
-                <button id="closeView" onclick="closeView()">x</button>
+        <div class="main">
+            <div class="bg">
+                    <!--blur Background image-->  
+            </div> 
+            <div>
+                <h1>Service Providers</h1>
+                <div id="hiddenView">
+                    <button id="closeView" onclick="closeView()">x</button>
+                    <center>
+                    <table>
+                        <tr><th class="c_th">Provider ID:</th> <td id="spId"></td></tr>
+                        <tr><th class="c_th">Username:</th> <td id="uName"></td></tr>
+                        <tr><th class="c_th">Full Name:</th> <td id="fName"></td></tr>
+                        <tr><th class="c_th">email:</th><td id="email"></td></tr>
+                        <tr><th class="c_th">Address:</th><td id="address"></td></tr>
+                        <tr><th class="c_th">Specialty:</th><td id="specialty"></td></tr>
+                        <tr><th class="c_th">Field:</th><td id="field"></td></tr>
+                    </table>
+                    </center>
+                    
+                </div>
                 <center>
-                <table>
-                    <tr><th class="c_th">Provider ID:</th> <td id="spId"></td></tr>
-                    <tr><th class="c_th">Username:</th> <td id="uName"></td></tr>
-                    <tr><th class="c_th">Full Name:</th> <td id="fName"></td></tr>
-                    <tr><th class="c_th">email:</th><td id="email"></td></tr>
-                    <tr><th class="c_th">Address:</th><td id="address"></td></tr>
-                    <tr><th class="c_th">Specialty:</th><td id="specialty"></td></tr>
-                    <tr><th class="c_th">Field:</th><td id="field"></td></tr>
-                </table>
-                </center>
-                
-            </div>
-            <center>
-                <table>
-                    <tr>
-                        <th>UId</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Specialty</th>
-                        <th>Actions</th>
-                    </tr>
-                <?php
-                    if ($result->num_rows > 0) {
-                        while($row = $result->fetch_assoc()) {
-                            echo "<tr><td>" . $row["provider_id"]. "</td><td>" . $row["username"]. "</td><td>" . $row["email"]. "</td><td>" . $row["speciality"]."</td><td class=\"actions\"><center><button class=\"view\" onclick=\"viewSp(".$row["provider_id"].")\">View</button><button class=\"del\" onclick=\"deleteSp(".$row["provider_id"].")\">Delete</button></center></td></tr>";
+                    <table>
+                        <tr>
+                            <th>UId</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Actions</th>
+                        </tr>
+                    <?php
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr><td>" . $row["provider_id"]. "</td><td>" . $row["username"]. "</td><td>" . $row["email"]. "</td><td>" . $row["speciality"]."</td><td class=\"actions\"><center><button class=\"view\" onclick=\"viewSp(".$row["provider_id"].")\">View</button><button class=\"del\" onclick=\"deleteSp(".$row["provider_id"].")\">Delete</button></center></td></tr>";
+                            }
+                        } else {
+                            echo "0 results";
                         }
-                    } else {
-                        echo "0 results";
-                    }
-                    $conn->close();
-                ?>
-                </table>
-            </center>
+                        $conn->close();
+                    ?>
+                    </table>
+                </center>
+            </div>
         </div>
     </body>
 </html>
