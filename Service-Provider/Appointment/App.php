e7bb044311af38ac89e028ae7b8dae8de499440b
judeajanthan <judeajanthan07@gmail.com>
2024-11-27 15:25:51 +0530

    connected css file

--- Service-Provider/Appointment/App.php
+++ Service-Provider/Appointment/App.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EDSA Lanka Consultancy</title>
-    <link rel="stylesheet" href="Appo.css">
+    <link rel="stylesheet" href="App.css">
 </head>
 <body>
     <div class="container">
