63daa2fd66a63493e6daded31401a1134d02889e
Tharusha <nemanathatharusha@gmail.com>
2024-11-29 13:22:22 +0530

    Remove unused login and logout files; update footer iframe and add footer styles

--- user1/Login/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); 
-include '../connect/connect.php'; //connect to database
-
-    session_unset();
-    session_destroy();
-    header("Location: Login.php");
-    exit();
-?>
