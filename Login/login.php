3c5580f39fc569249e3fd18d72e98f8ef9b9d0d5
Tharusha <nemanathatharusha@gmail.com>
2024-11-29 14:58:40 +0530

    Fix: Correct file path for service provider dashboard in login script

--- Login/login.php
+++ Login/login.php
@@ -21,7 +21,7 @@ if (isset($_POST['submit'])) { //check if form was submitted
         'admin' => '../Admin/admin/dashboard/admin.php',
         'companyworker' => '../companyworkers/dashboard/dashboard.html',
         'client' => '../user1/user/Dashboard/Dashboard.php',
-        'serviceprovider' => '../Service-Provider/SP_Dashboard/SP_Dash.html'
+        'serviceprovider' => '../Service-Provider/SP_Dashboard/SPDash.html'
     ];
 
     $userType = null;
