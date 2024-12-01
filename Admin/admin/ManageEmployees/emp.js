fc5d4cb1df89588b75b9b35e049df14fdf76d5cd
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-12-01 12:21:58 +0530

    Add functions to show and close employee form in emp.js

--- Admin/admin/ManageEmployees/emp.js
+++ Admin/admin/ManageEmployees/emp.js
@@ -1,3 +1,10 @@
 function closeView(){
     document.getElementById('results').style.display = "none";
 }
+
+function closeForm(){
+    document.getElementById('addEmp').style.display = "none";
+}
+function showForm(){
+    document.getElementById('addEmp').style.display = "block";
+}
