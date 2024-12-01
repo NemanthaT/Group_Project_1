597878203f28fbd918ea848c16224c7c8d6c7681
Tharusha <nemanathatharusha@gmail.com>
2024-12-01 12:46:20 +0530

    Enhance employee form visibility by adding blur effect to main content on show/hide actions

--- Admin/admin/ManageEmployees/emp.js
+++ Admin/admin/ManageEmployees/emp.js
@@ -4,7 +4,9 @@ function closeView(){
 
 function closeForm(){
     document.getElementById('addEmp').style.display = "none";
+    document.getElementById('main').style.filter = "blur(0px)";
 }
 function showForm(){
     document.getElementById('addEmp').style.display = "block";
+    document.getElementById('main').style.filter = "blur(10px)";
 }
