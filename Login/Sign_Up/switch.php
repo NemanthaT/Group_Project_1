80ec63a160c3e53a245df6bce23051908ec52d33
Tharusha <nemanathatharusha@gmail.com>
2025-01-31 15:39:14 +0530

    style: update switch.php with header changes and improved navigation links

--- Login/Sign_Up/switch.php
+++ Login/Sign_Up/switch.php
@@ -25,32 +25,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'  && isset($_POST['sP'])) {
     <div class="blurry-background"></div>
     <div class="container">
         <div class="form-header">
-            <h2>Select Option</h2>
+            <h1>Choose Account Type</h1>
         </div>
         <div class="forms">
-            <form action="" class="client" method="post">
-                <div >
+            <form action="" class="client" id="cl" method="post">
+                <div onclick="window.location.href='Sign_UpC.php'">
                     <center>
                         <h2>Client</h2>
-                        <span style='font-size:100px;'>&#128129;</span><br>
-                        <button name="sC" type="submit">Signup</button>
+                        <span style='font-size:200px;'>&#128129;</span><br>
+                        <!--<button name="sC" type="submit">Signup</button>-->
                     </center>
 
                 </div>
             </form>
-            <form action="" class="provider" method="post">
-                <div >
+            <form action="" class="provider" id="prov" method="post">
+                <div onclick="window.location.href='Sign_UpP.php'">
                     <center>
                         <h2>Service Provider</h2>
-                        <span style='font-size:100px;'>&#129333;</span><br>
-                        <button name="sP" type="submit">Signup</button>
+                        <span style='font-size:200px;'>&#129333;</span><br>
+                        <!--<button name="sP" type="submit">Signup</button>-->
                     </center>
 
                 </div>
             </form>
         </div>
         <div class="form-footer">
-            <button onclick="window.location.href='../login.php'" class="bBtn">Back</button>
+            <!--<button onclick="window.location.href='../login.php'" class="bBtn">Back</button>-->
+            <center><p class="backLink">&#11164; <a href="../login.php">Back to login</a></p></center>
         </div>
                                                                                                  
     </div>
