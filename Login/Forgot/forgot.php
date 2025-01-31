9d85e7e057ed698c33284d4b43bef169f08dc685
Tharusha <nemanathatharusha@gmail.com>
2025-01-31 12:43:36 +0530

    style: redesign forgot password page layout and enhance user interaction

--- Login/Forgot/forgot.php
+++ Login/Forgot/forgot.php
@@ -9,28 +9,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="../login.css">
+    <link rel="stylesheet" href="forgot.css">
 </head>
 
 <body>
     <div class="blurry-background"></div>
     <div class="container">
-        <form action="login.php" method="post">
-            <div class="form-header">
-                <h2>Forgot Password</h2>
-            </div>
-            <div>
-                <p style="margin: 5px 10px 10px 10px">If you are a registered user an e-mail will be sent your e-mail address.</p>
-            </div>
-            <div class="form-group1">
-            <p style="margin: 5px 10px 10px 10px">Please enter your registered e-mail.</p>
-                <input type="text" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <center>
-                    <button name="submit" type="submit" style="margin-bottom: 5px">Send</button>
-                </center>
-            </div>
-        </form>
+        <div class="conLeft">
+            <center>
+                <img class="loginImg" src="forgot.png" alt="login">
+            </center>
+        </div>
+        <div class="conRight">
+            <h1>Forgot<br> Password?</h1>
+            <p>If you are a registered user, reset link will be sent to your e-mail address.</p>
+            <p>Please enter your registered e-mail.</p>
+            <form action="login.php" method="post">
+                <div>
+                    <input type="text" id="email" name="email" placeholder=" &#128231; E-mail" required>
+                </div>
+                <div class="form-group">
+                        <button name="submit" type="submit" style="margin-bottom: 5px">Send Reset Link</button>
+                </div>
+            </form>
+            <center><p class="backLink">&#11164; <a href="../login.php">Back to login</a></p></center>
+        </div>
     </div>
     <footer>
         <p>&copy; 2024 EDSA Lanka Consultancy</p>
