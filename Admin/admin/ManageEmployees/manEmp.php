27405f63f151bba8cb1ede3fe191116a56010184
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-04-08 01:12:08 +0530

    fix: update hideList function logic and remove onclick from search button

--- Admin/admin/ManageEmployees/manEmp.php
+++ Admin/admin/ManageEmployees/manEmp.php
@@ -76,7 +76,7 @@
                     <div class="searchContainer">
                         <form action="" method="POST">
                             <input type="text" name="name" placeholder="Enter Name" required>
-                            <button class="sBtn" type="submit" name="search_name" onclick="hideList()">Search</button>
+                            <button class="sBtn" type="submit" name="search_name">Search</button>
                         </form>
                     </div>
                 </div>
