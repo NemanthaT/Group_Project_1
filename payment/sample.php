a956ab6328fc17613df06f04d0466cbd5dcfb98d
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-04-13 17:07:58 +0530

    feat: create payment sample HTML page with payment button

---
+++ payment/sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Payment Sample</title>
+        <script src="pay.js"></script>
+        <script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
+    </head>
+
+    <body>
+        <div>
+            <button onclick="paymentGateway()">Pay</button>
+        </div>
+    </body>
+</html>
