45362adf571ba095e73fab8752c07bd355c82d3f
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-04-21 22:16:25 +0530

    Update config file path in employee management PHP files

--- Admin/admin/ManageEmployees/addEmp.php
+++ Admin/admin/ManageEmployees/addEmp.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../../config/config.php');
+    require_once('../../../config/config.php');
     header('Content-Type: application/json');
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
--- Admin/admin/ManageEmployees/rmEmp.php
+++ Admin/admin/ManageEmployees/rmEmp.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../../config/config.php');
+    require_once('../../../config/config.php');
     $afDiv = "mainContent";
     header('Content-Type: application/json');
 
