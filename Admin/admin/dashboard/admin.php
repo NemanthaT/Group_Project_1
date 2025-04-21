bf95c99da01660a475f0f4ff25a5ee19acd1e9e1
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-04-21 22:15:45 +0530

    Update config file path in admin dashboard files

--- Admin/admin/dashboard/admin.php
+++ Admin/admin/dashboard/admin.php
@@ -1,6 +1,6 @@
 <?php
     session_start(); 
-    require_once('../../config/config.php');
+    require_once('../../../config/config.php');
 
     $username = $_SESSION['username'];
     $email = $_SESSION['email'];
