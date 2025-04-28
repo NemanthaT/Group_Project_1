a51f37e1ac44782c7b11a27921267de3addb6ea7
Tharusha Nemantha <nemanathatharusha@gmail.com>
2025-04-29 01:46:43 +0530

    fix: update forum details to include creator's full name and creation date

--- Admin/admin/Forums/view_forum.php
+++ Admin/admin/Forums/view_forum.php
@@ -12,7 +12,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $result = $stmt->get_result();
 
     if ($row = $result->fetch_assoc()) {
-        echo json_encode($row);
+        if($row['client_id']==NULL){
+            $stmt = $conn->prepare("SELECT full_name FROM serviceproviders WHERE provider_id = ?");
+            $stmt->bind_param("i", $row['provider_id']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row2 = $result->fetch_assoc();
+            $row['full_name'] = $row2['full_name'];
+            echo json_encode($row);
+        }
+        else{
+            $stmt = $conn->prepare("SELECT full_name FROM clients WHERE client_id = ?");
+            $stmt->bind_param("i", $row['client_id']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row2 = $result->fetch_assoc();
+            $row['full_name'] = $row2['full_name'];
+            echo json_encode($row);
+        }
     } else {
         echo json_encode(["error" => "No forum found with this ID."]);
     }
