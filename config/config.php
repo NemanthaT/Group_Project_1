b8155cd7a1de68fee7295b80e8c3d418fef5aba7
Tharusha <nemanathatharusha@gmail.com>
2025-04-29 15:23:14 +0530

    Refactor database configuration: update connection parameters to use local settings and improve code clarity

--- config/config.php
+++ config/config.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "mysql-edsa.alwaysdata.net";
+$db_host = 'localhost'; // database host
+$db_name = 'edsaLanka'; // database name
+$db_user = 'root'; // database username
+$db_pass = ''; // database password
+
+/*$servername = "mysql-edsa.alwaysdata.net";
 $username = "edsa";
 $password = "123@safran"; 
-$dbname = "edsa_lanka"; 
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$dbname = "edsa_lanka"; */
 
 
-// Check connection
+// Connect to the database
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+/*$conn = new mysqli($servername, $username, $password, $dbname);*/
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
