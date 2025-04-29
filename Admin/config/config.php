24cee5638c040d2b232bde89221816d7f44dc94e
Tharusha <nemanathatharusha@gmail.com>
2025-04-29 15:15:25 +0530

    Refactor database configuration: uncomment database connection code and remove unused variables

--- Admin/config/config.php
+++ Admin/config/config.php
@@ -1,19 +1,19 @@
 <?php
-/*$db_host = 'localhost'; // database host
+$db_host = 'localhost'; // database host
 $db_name = 'edsaLanka'; // database name
 $db_user = 'root'; // database username
-$db_pass = ''; // database password*/
+$db_pass = ''; // database password
 
-$servername = "mysql-edsa.alwaysdata.net";
+/*$servername = "mysql-edsa.alwaysdata.net";
 $username = "edsa";
 $password = "123@safran"; 
-$dbname = "edsa_lanka"; 
+$dbname = "edsa_lanka"; */
 
 
 // Connect to the database
-/*$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);*/
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+/*$conn = new mysqli($servername, $username, $password, $dbname);*/
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
